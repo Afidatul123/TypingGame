3c05299ba68ccd2791fbb1c3cf7c1eee64821f74
sitompuledward180801@hotmail.com <sitompuledward180801@hotmail.com>
2023-01-15 23:01:57 +0700

    Add project files.

---
+++ TypingGame/Models/Stats.cs
@@ -0,0 +1,26 @@
+﻿namespace TypingGame.Models
+{
+	public class Stats
+	{
+		public int Correct { get; set; }
+		public int Missed { get; set; }
+		public int Total { get
+			{
+				return Correct + Missed; //bisa langsung di custom
+			}
+			set { } } 
+		//bisa ditambah private di set agar hanya bisa di get jadi ngga bisa di set lagi
+		public int Accuracy { get; set; }
+
+		//mengitung Total klik
+		public void CountTotal()
+		{
+			Total = Correct + Missed;
+		}
+
+		public void CountAccuracy()
+		{
+			Accuracy = Correct * 100 / (Correct + Missed);
+		}
+	}
+}
